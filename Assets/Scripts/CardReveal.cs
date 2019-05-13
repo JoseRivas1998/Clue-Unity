19cd2cb633ed79dd044eaf37d77117ea2b7456cd
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-05-13 10:23:46 -0700

    Spinning Card Reveal

---
+++ Assets/Scripts/CardReveal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardReveal : MonoBehaviour
+{
+
+    public float degreesPerSecond = -180;
+
+    [HideInInspector]
+    public Texture2D target = null;
+
+    private enum CardRevealState
+    {
+        Inactive,
+        IsRevealing,
+        IsDone
+    }
+
+    private CardRevealState state;
+    private bool hasSwitched;
+    private RawImage img;
+    private RectTransform rectTrans;
+
+    private void Start()
+    {
+        hasSwitched = false;
+        img = GetComponent<RawImage>();
+        rectTrans = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if(state.Equals(CardRevealState.IsRevealing))
+        {
+            rectTrans.Rotate(0, degreesPerSecond * Time.deltaTime, 0);
+            if(rectTrans.eulerAngles.y <= 90.5 && !hasSwitched)
+            {
+                img.texture = target;
+            }
+            if(rectTrans.eulerAngles.y <= 0.5)
+            {
+                rectTrans.eulerAngles = new Vector3(0, 0, 0);
+                state = CardRevealState.IsDone;
+            }
+        }
+    }
+
+
+    public void Activate()
+    {
+        if (state.Equals(CardRevealState.Inactive) && target != null)
+        {
+            state = CardRevealState.IsRevealing;
+        }
+    }
+
+    public bool IsDone()
+    {
+        return state.Equals(CardRevealState.IsDone);
+    }
+}
--- Assets/Scripts/SolutionDisplay.cs
+++ Assets/Scripts/SolutionDisplay.cs
@@ -8,23 +8,40 @@ public class SolutionDisplay : MonoBehaviour
 {
 
     public Text solutionText;
-    public RawImage character;
-    public RawImage weapon;
-    public RawImage room;
+
+    public CardReveal character;
+    public CardReveal weapon;
+    public CardReveal room;
+
+    private int currentActive;
 
     // Start is called before the first frame update
     void Start()
     {
-        character.texture = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Character);
-        weapon.texture = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Weapon);
-        room.texture = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Room);
+        character.target = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Character);
+        weapon.target = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Weapon);
+        room.target = CharacterResourceManager.CardImageTexture(ClueData.Instance.Solution.Room);
         solutionText.text = "Player " + ClueData.Instance.PlayerAccusation + " Was " + (ClueData.Instance.Solution.Equals(ClueData.Instance.Guess) ? "Correct!" : "Incorrect!");
+        currentActive = 0;
+        character.Activate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+        if(currentActive == 0 && character.IsDone())
+        {
+            currentActive++;
+            weapon.Activate();
+        }
+
+        if(currentActive == 1 && weapon.IsDone())
+        {
+            currentActive++;
+            room.Activate();
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space) && room.IsDone())
         {
             SceneManager.LoadScene(0);
         }
