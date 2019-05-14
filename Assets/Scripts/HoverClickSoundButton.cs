aec737fbe25b917327fbb323e479719681c0b3aa
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-05-14 10:16:18 -0700

    Menu

---
+++ Assets/Scripts/HoverClickSoundButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverClickSoundButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+{
+
+    public AudioSource cursorSource;
+
+    public AudioClip cursor;
+    public AudioClip choice;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        cursorSource.clip = choice;
+        cursorSource.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        cursorSource.clip = cursor;
+        cursorSource.Play();
+    }
+}
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public Button playButton;
+    public Button backButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Space))
-        {
-            SceneManager.LoadScene("SelectCharacter");
-        }
+        playButton.onClick.AddListener(() => { SceneManager.LoadScene("SelectCharacter"); });
     }
 }
